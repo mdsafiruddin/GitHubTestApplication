19b053cbf9a7bebed231d15f1c02e0483e7ce60e
MD <MD@Safu>
2019-07-25 21:49:11 +0530

    gitTest

---
+++ src/com/app/GitTest.java
@@ -0,0 +1,54 @@
+package com.app;
+
+public class GitTest {
+
+	private int gid;
+	private String hubName;
+	private String hubLoc;
+	
+	
+	public GitTest() {
+		super();
+	}
+
+
+	public int getGid() {
+		return gid;
+	}
+
+
+	public void setGid(int gid) {
+		this.gid = gid;
+	}
+
+
+	public String getHubName() {
+		return hubName;
+	}
+
+
+	public void setHubName(String hubName) {
+		this.hubName = hubName;
+	}
+
+
+	public String getHubLoc() {
+		return hubLoc;
+	}
+
+
+	public void setHubLoc(String hubLoc) {
+		this.hubLoc = hubLoc;
+	}
+
+
+	@Override
+	public String toString() {
+		return "GitTest [gid=" + gid + ", hubName=" + hubName + ", hubLoc=" + hubLoc + "]";
+	}
+	
+	
+	
+	
+	
+}
